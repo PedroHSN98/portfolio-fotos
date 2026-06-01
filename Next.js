932bb7cfdc81c52e1e3bba582755284aa942cc09
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2026-06-01 08:19:57 -0400

    V1.3

--- Next.js
+++ Next.js
@@ -1,1 +1,12 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+
+export default function ComponenteLogado() {
+  return (
+    <div>
+      {/* O restante do seu código existente entra aqui */}
+      
+      {/* Insira a tag do Vercel Speed Insights logo antes de fechar a div principal */}
+      <SpeedInsights />
+    </div>
+  )
+}
